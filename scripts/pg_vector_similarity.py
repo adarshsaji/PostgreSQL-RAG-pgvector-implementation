04131920bed677abbc72410073cb17f140c1fcf4
Adarsh <43480668+adarshsaji@users.noreply.github.com>
2024-12-16 00:11:29 +0000

    minor updates

--- scripts/pg_vector_similarity.py
+++ scripts/pg_vector_similarity.py
@@ -34,8 +34,8 @@ CONNECTION_STRING = PGVector.connection_string_from_db_params(
     driver=os.environ.get("PGVECTOR_DRIVER", "psycopg2"),
     host=os.environ.get("PGVECTOR_HOST", "localhost"),
     port=int(os.environ.get("PGVECTOR_PORT", "5432")),
-    database=os.environ.get("PGVECTOR_DATABASE", "fluck-cb-db"),
-    user=os.environ.get("PGVECTOR_USER", "fluck-chat-user"),
+    database=os.environ.get("PGVECTOR_DATABASE", "chat-db"),
+    user=os.environ.get("PGVECTOR_USER", "chat-user"),
     password=os.environ.get("PGVECTOR_PASSWORD", "temp-password"),
 )
 
